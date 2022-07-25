a31ed2733ecd65a5a1d53a73979ccf386e29ef6d
gary <gary.white@lodgit.com>
2022-07-25 12:57:13 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function getDivisorsCnt(n) {
+    // todo
+}
+
+console.log(getDivisorsCnt(1)); // 1
+console.log(getDivisorsCnt(10)); // 4
+console.log(getDivisorsCnt(11)); // 2
+console.log(getDivisorsCnt(54)); // 8
